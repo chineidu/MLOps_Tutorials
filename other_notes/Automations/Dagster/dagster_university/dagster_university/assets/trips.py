9e5a3667915de1db83ca53fc57ae3901e1394b30
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2025-02-02 06:47:46 +0100

    chore: Update code

--- other_notes/Automations/Dagster/dagster_university/dagster_university/assets/trips.py
+++ other_notes/Automations/Dagster/dagster_university/dagster_university/assets/trips.py
@@ -6,8 +6,9 @@ from dagster import asset
 
 @asset
 def taxi_trips_file() -> None:
+    """Download taxi trip data from NYC Open Data and save to local file."""
     month_to_fetch: str = "2023-03"
-    raw_trips = requests.get(
+    raw_trips: requests.Response = requests.get(
         (
             f"https://d37ci6vzurychx.cloudfront.net/"
             f"trip-data/yellow_tripdata_{month_to_fetch}.parquet"
@@ -16,3 +17,14 @@ def taxi_trips_file() -> None:
 
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as f:
         f.write(raw_trips.content)
+
+
+@asset
+def taxi_zones_file() -> None:
+    """Download NYC taxi zones data rom NYC Open Data Portal and save to local file."""
+    taxi_zones: requests.Response = requests.get(
+        "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as f:
+        f.write(taxi_zones.content)
