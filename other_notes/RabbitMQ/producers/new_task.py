10cbbb1188f4741a054d23b52dfb5fd24c97daeb
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2025-02-23 20:56:18 +0100

    chore: Code cleanup and refactoring

--- other_notes/RabbitMQ/producers/new_task.py
+++ other_notes/RabbitMQ/producers/new_task.py
@@ -4,26 +4,37 @@ import sys
 import pika
 from utilities import logger
 
-# Connect to RabbitMQ server running on localhost
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-channel = connection.channel()
 
-# Create a queue
-channel.queue_declare(
-    queue="hello",
-    durable=True,  # Make the queue durable
-)
+def main() -> None:
+    # Connect to RabbitMQ server running on localhost
+    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+    channel = connection.channel()
 
-# Send a message to the queue using the default exchange
-message: str = " ".join(sys.argv[1:]) or "Hello World!"
-channel.basic_publish(
-    exchange="",  # Use the default exchange
-    routing_key="hello",  # The name of the queue
-    body=message,  # The message to send
-    properties=pika.BasicProperties(
-        delivery_mode=pika.DeliveryMode.Persistent,  # Make the message persistent
-    ),
-)
-logger.info(f" [x] Sent {message}")
-# Close the connection
-connection.close()
+    # Create a queue
+    channel.queue_declare(
+        queue="hello",
+        durable=True,  # Make the queue durable
+    )
+
+    # Send a message to the queue using the default exchange
+    message: str = " ".join(sys.argv[1:]) or "Hello World!"
+    channel.basic_publish(
+        exchange="",  # Use the default exchange
+        routing_key="hello",  # The name of the queue
+        body=message,  # The message to send
+        properties=pika.BasicProperties(
+            # Make the message persistent
+            delivery_mode=pika.DeliveryMode.Persistent,
+        ),
+    )
+    logger.info(f" [x] Sent {message}")
+    # Close the connection
+    connection.close()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.info("Interrupted")
+        sys.exit(0)
