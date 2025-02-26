903feaa95993bf1322d98c3541e58145a28f18ea
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2025-02-26 14:00:49 +0100

    chore: RabbitMQ Async programming

---
+++ other_notes/RabbitMQ/Async/Producers/send.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from aio_pika import Message, connect
+from utilities import logger
+
+username: str = "guest"
+password: str = "guest"
+host: str = "localhost"
+URL: str = f"amqp://{username}:{password}@{host}/"
+queue_name: str = "hello"
+logger.info("Starting producer")
+
+
+async def main() -> None:
+    connection = await connect(URL)
+
+    async with connection:
+        # Create a channel
+        channel = await connection.channel()
+        # Declare queue
+        queue = await channel.declare_queue(queue_name)
+        # Send a message
+        await channel.default_exchange.publish(
+            Message(b"Hello World!"),
+            routing_key=queue.name,
+        )
+        logger.info(" [x] Sent 'Hello World!'")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
